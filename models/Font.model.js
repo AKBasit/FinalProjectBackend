ccfb806e785c999daaca0bfe1c27f5f8b37484e6
Ian IJmkers <ianymkers@gmail.com>
2024-01-17 23:05:47 +0100

    worked on modelschema for when we deal with image variables

--- models/Font.model.js
+++ models/Font.model.js
@@ -3,11 +3,11 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const fontSchema = new Schema(
   {
-    // owner: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User",
-    //   required: true,
-    // },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      //   required: true,
+    },
     name: {
       type: String,
       required: [true, "font name is required"],
